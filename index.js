b4b30375fdf1342970d1764f785fd3bfc3347024
James Cuzella <james.cuzella@lyraphase.com>
2023-10-17 14:11:44 -0600

    Generate static site

--- index.js
+++ index.js
@@ -11,4 +11,12 @@ const templateHTML = fs.readFileSync(path.join(__dirname, 'dist', 'template.html
 
 for (let [slug, url] of Object.entries(redirects)) {
     console.log('Generating HTML page for ', slug, ' = ', url);
+    const html = templateHTML.replaceAll('https://example.com', url);
+
+    // create folder for each slug
+    const folderPath = path.join(__dirname, 'out', slug);
+    fs.mkdirSync(folderPath, { recursive: true });
+
+    // Create index.html for each folder
+    fs.writeFileSync(path.join(folderPath, 'index.html'), html);
 }
